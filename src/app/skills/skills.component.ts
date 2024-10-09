ba835e3fb4b339e32771a2a37f6f0718b27e6931
Jennifer Petrus <jenny.petrus19@gmail.com>
2024-10-09 14:33:13 +0200

    Translation, Navbar, Title, About

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  skills: any = [
+    {logo: '/assets/img/icons/angular-big.png',description: 'Angular'},
+    {logo: '/assets/img/icons/typescript-big.png',description: 'Typescript'},
+    {logo: '/assets/img/icons/javascript-big.png',description: 'Javascript'},
+    {logo: '/assets/img/icons/html5-big.png',description: 'HTML5'},
+    {logo: '/assets/img/icons/firebase-big.png',description: 'Firebase'},
+    {logo: '/assets/img/icons/api-big.png',description: 'API'},
+    {logo: '/assets/img/icons/css-big.png',description: 'CSS'},
+    {logo: '/assets/img/icons/material-design-big.png',description: 'Material Design'},
+    {logo: '/assets/img/icons/bootstrap.svg',description: 'Bootstrap'},
+    {logo: '/assets/img/icons/git.png',description: 'GIT'},
+    {logo: '/assets/img/icons/scrum.png',description: 'Scrum'},
+    {logo: '/assets/img/icons/learning.svg', description: 'Continually <br> learning'},
+  ]
 
+  constructor() { }
+
+  getSkills() {
+    return this.skills;
+  }
 }
--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './title.component.scss'
 })
 export class TitleComponent {
-
+  bgLeft: string = "/assets/img/bg/intro-blue-shadow.png";
+  bgRight: string = "/assets/img/bg/intro-darkblue-shadow.png";
 }
