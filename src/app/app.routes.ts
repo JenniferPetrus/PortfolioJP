b642681494b2d8c5cdadbef93b72056530cb82f6
Jennifer Petrus <jenny.petrus19@gmail.com>
2024-10-14 10:56:21 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: AboutComponent },
     { path: '**', component: PageNotFoundComponent } 
 ];
