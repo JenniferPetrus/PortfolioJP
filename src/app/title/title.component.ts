d501df06137df5888e8ee81f275110c1c77a3d7a
Jennifer Petrus <jenny.petrus19@gmail.com>
2024-10-14 20:24:21 +0200

    update Arrow Up

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslationService } from '../translation.service';
 
@@ -7,21 +7,36 @@ import { TranslationService } from '../translation.service';
   standalone: true,
   imports: [TranslateModule],
   templateUrl: './title.component.html',
-  styleUrl: './title.component.scss'
+  styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
   translate = inject(TranslationService);
   bgLeft: string = "/assets/img/bg/intro-blue-shadow.png";
   bgRight: string = "/assets/img/bg/intro-darkblue-shadow.png";
 
+  // Scroll to a specific element
   scrollToElement(elementId: string): void {
     const element = document.getElementById(elementId);
     if (element) {
       const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-      const offsetPosition = elementPosition - 200; 
+      const offsetPosition = elementPosition - 200;
       window.scrollTo({
         top: offsetPosition,
         behavior: 'smooth'
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    const goUp = document.querySelector(".go-up") as HTMLElement | null;
+
+    if (goUp) {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+          goUp.classList.add("active");
+        } else {
+          goUp.classList.remove("active");
+        }
       });
     }
   }
